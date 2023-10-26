16f7bb0766446e86f6a0182707696a219fb856fd
douglasjansey <douglas.jansey@gmail.com>
2023-10-26 10:18:04 -0300

    vercel

--- src/app/index.ts
+++ src/app/index.ts
@@ -10,4 +10,4 @@ app.use(cors())
 app.use(express.json())
 app.use('/product', router);
 
-app.listen(3001, () => console.log("serverRun"));
+app.listen(process.env.PORT || 3001, () => console.log("serverRun"));
